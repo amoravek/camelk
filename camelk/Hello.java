ab7d7ecc007dc1f26d3e59ef3bb96e6f240c1b3f
amoravek <amoravek@gmail.com>
2023-02-23 22:59:42 +0100

    updates

---
+++ camelk/Hello.java
@@ -0,0 +1,8 @@
+import org.apache.camel.builder.RouteBuilder;
+
+public class Hello extends RouteBuilder {
+  @Override
+  public void configure() throws Exception {
+    from("timer:tick?period=3000").setBody().constant("Hello world from Camel K").to("log:info");
+  }
+}
